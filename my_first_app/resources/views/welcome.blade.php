4ed5be6d0aa480dd5c1d187dd211f8f5143154a2
Nathan Dolor <natebdolor@gmail.com>
2023-04-19 07:12:38 -0400

    Deleted unnecessary files and updated welcome page

--- my_first_app/resources/views/welcome.blade.php
+++ my_first_app/resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
 @section('content')
     <div class="vh-100 d-flex align-items-center">
         <div class="d-grid gap-2 col-3 mx-auto align-items-center justify-content-center" >    
-            <img src="{{ asset('favicon.png') }}" alt="BookIt Logo" width="25%" style="margin-bottom: 20px;" class="mx-auto">
+            <img src="{{ asset('favicon.png') }}" alt="BookIt Logo" width="25%" class="mx-auto">
+            <h2 style="margin-bottom: 20px; text-align: center">Welcome to, <span style="color: #0d6efd;">Book</span><span style="color: #FF0000;">It</span>!</h2>
             <a class="btn btn-outline-primary" href="{{ url(route('login')) }}" role="button" style="border-width: 3px;"><strong>Login</strong></a>
             <a class="btn custom-button" href="{{ url(route('registration')) }}" role="button">Register</a>        
         </div>
